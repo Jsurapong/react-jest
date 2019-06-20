5e041ffac2ac3792b47caa9986175da8d7118022
Surapong Kawkangploo <surapongkawkangploo@Surapongs-MacBook-Pro.local>
2019-06-20 18:51:27 +0700

    user

--- test/Users.test.js
+++ test/Users.test.js
@@ -10,30 +10,22 @@ const response = {
 
 jest.mock('axios');
 
-// describe('Users component', () => {
-//   it('shows a list of users', async () => {
-//     axios.get.mockResolvedValue(response);
-
-//     const component = create(<Users />);
-//     const instance = component.getInstance();
-//     await instance.componentDidMount();
-//     const root = component.root;
-
-//     const listOfLi = root.findAll(element => element.type === 'li');
-
-//     expect(listOfLi[0].props.children).toBe('Kevin Mitnick');
-//     expect(listOfLi[1].props.children).toBe('Valentino Gagliardi');
-//   });
-// });
-
-describe('User Component', () => {
-  it('shows a list of users', async () => {
+describe('Users component', () => {
+  beforeEach(() => {
     axios.get.mockResolvedValue(response);
+  });
+
+  it('shows a list of users', async () => {
     const component = create(<Users />);
     const instance = component.getInstance();
 
-    await instance.getUser();
+    await instance.componentDidMount();
 
-    console.log(instance.state);
+    const root = component.root;
+
+    const listOfLi = root.findAll(element => element.type === 'li');
+
+    expect(listOfLi[0].props.children).toBe('Kevin Mitnick');
+    expect(listOfLi[1].props.children).toBe('Valentino Gagliardi');
   });
 });
